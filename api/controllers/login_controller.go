1a80ea89ab6aa3dba445e960edec9ef3e1551c59
haryfun <hary.vai@gmail.com>
2021-03-09 02:04:35 +0700

    create ligin controller and method login

---
+++ api/controllers/login_controller.go
@@ -0,0 +1,44 @@
+package controllers
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/haryanapnx/go-blog-crud/api/models"
+	"github.com/haryanapnx/go-blog-crud/api/responses"
+	"github.com/haryanapnx/go-blog-crud/api/utils/formaterror"
+)
+
+func (server *Server) Login(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	user := models.User{}
+	err = json.Unmarshal(body, &user)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	user.Prepare()
+	err = user.Validate("login")
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+	token, err := server.SignIn(user.Email, user.Password)
+	if err != nil {
+		formattedError := formaterror.FormatError(err.Error())
+		responses.ERROR(w, http.StatusUnprocessableEntity, formattedError)
+		return
+	}
+	responses.JSON(w, http.StatusOK, token)
+}
+
+func (server *Server) SignIn(email string, password string) {
+
+}
