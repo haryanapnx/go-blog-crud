fc94c990d696a4b37ce4b9c72dbe8b6a5a303f14
haryfun <hary.vai@gmail.com>
2021-03-09 02:17:17 +0700

    create method signin

--- api/controllers/login_controller.go
+++ api/controllers/login_controller.go
@@ -5,9 +5,11 @@ import (
 	"io/ioutil"
 	"net/http"
 
+	"github.com/haryanapnx/go-blog-crud/api/auth"
 	"github.com/haryanapnx/go-blog-crud/api/models"
 	"github.com/haryanapnx/go-blog-crud/api/responses"
 	"github.com/haryanapnx/go-blog-crud/api/utils/formaterror"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func (server *Server) Login(w http.ResponseWriter, r *http.Request) {
@@ -16,7 +18,6 @@ func (server *Server) Login(w http.ResponseWriter, r *http.Request) {
 		responses.ERROR(w, http.StatusUnprocessableEntity, err)
 		return
 	}
-
 	user := models.User{}
 	err = json.Unmarshal(body, &user)
 	if err != nil {
@@ -39,6 +40,19 @@ func (server *Server) Login(w http.ResponseWriter, r *http.Request) {
 	responses.JSON(w, http.StatusOK, token)
 }
 
-func (server *Server) SignIn(email string, password string) {
+func (server *Server) SignIn(email, password string) (string, error) {
 
+	var err error
+
+	user := models.User{}
+
+	err = server.DB.Debug().Model(models.User{}).Where("email = ?", email).Take(&user).Error
+	if err != nil {
+		return "", err
+	}
+	err = models.VerifyPassword(user.Password, password)
+	if err != nil && err == bcrypt.ErrMismatchedHashAndPassword {
+		return "", err
+	}
+	return auth.CreateToken(user.ID)
 }
