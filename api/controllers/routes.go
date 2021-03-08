012723ab2d83d096e10f9369d48f26fb81d506c9
haryfun <hary.vai@gmail.com>
2021-03-09 02:26:47 +0700

    register user router

--- api/controllers/routes.go
+++ api/controllers/routes.go
@@ -6,4 +6,11 @@ func (s *Server) initializeRoutes() {
 
 	// HEALTH CHECK
 	s.Router.HandleFunc("/health-check", middlewares.SetMiddlewareJSON(s.HealthCheck)).Methods("GET")
+
+	//USER
+	s.Router.HandleFunc("/users", middlewares.SetMiddlewareJSON((s.GetAllUser))).Methods("GET")
+	s.Router.HandleFunc("/users", middlewares.SetMiddlewareJSON(s.CreateUser)).Methods("POST")
+	s.Router.HandleFunc("/users/{id}", middlewares.SetMiddlewareJSON(s.GetUser)).Methods("GET")
+	s.Router.HandleFunc("/users/{id}", middlewares.SetMiddlewareJSON(middlewares.SetMiddlewareAuthentication(s.UpdatedUser))).Methods("PUT")
+	s.Router.HandleFunc("/users/{id}", middlewares.SetMiddlewareAuthentication(s.RemoveUser)).Methods("DELETE")
 }
