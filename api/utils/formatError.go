3563404f22584bd7116691606fa6369d7c040261
haryfun <hary.vai@gmail.com>
2021-03-08 17:22:42 +0700

    create error formatter

---
+++ api/utils/formatError.go
@@ -0,0 +1,27 @@
+package formatError
+
+import (
+	"errors"
+	"strings"
+)
+
+func FormatError(err string) error {
+	var msg string = "already taken"
+
+	if strings.Contains(err, "username") {
+		return errors.New("Username" + msg)
+	}
+
+	if strings.Contains(err, "email") {
+		return errors.New("Email" + msg)
+	}
+
+	if strings.Contains(err, "title") {
+		return errors.New("Title" + msg)
+	}
+
+	if strings.Contains(err, "hashedPassword") {
+		return errors.New("Incorect Password")
+	}
+	return errors.New("Incorect Detail")
+}
